8299421c3d2b4e62cb73489a2a0da3c39711bb6f
fabio-12 <128527959+fabio-12@users.noreply.github.com>
2023-12-04 18:44:05 +0100

    lavoro fatto dal migliore

---
+++ app/src/main/java/com/example/is_poi/Alberghi.java
@@ -0,0 +1,53 @@
+package com.example.is_poi;
+
+import java.util.Vector;
+
+public class Alberghi{
+    String PROVINCIA;
+    String COMUNE="bella";
+    String LOCALITA;
+    String TIPOLOGIA;
+    String TIPOLOGIASECONDARIA;
+    String DENOMINAZIONE;
+    String INDIRIZZO;
+    String NUMERO;
+    String CIVICO;
+    String INTERNO;
+    String CAP;
+    String TELEFONO;
+    String FAX;
+    String EMAIL;
+    String SITOWEB;
+    String ZONA;
+    String PISCINA;
+    String PISCINACOPERTA;
+    String RISTORANTE;
+    String PARCHEGGIO;
+    String SALACONFERENZE;
+    String ARIACONDIZIONATA;
+    String GIOCHIBIMBI;
+    String SAUNA;
+    String SOLARIUM;
+    String FITNESS;
+    String ANIMALIAMMESSI;
+    String CENTROSTORICO;
+    String ZONAFIERA;
+    String LAGO;
+    String AEROPORTO;
+    String AUTOSTRADA;
+    String IMPIANTIRISALITA;
+    String STAZIONEFS;
+    String MARE;
+    String TERMALE;
+    String PERIFERIA;
+    String COLLINARE;
+    String INGLESE;
+    String FRANCESE;
+    String TEDESCO;
+    String SPAGNOLO;
+    String CHIUSURATEMPORANEA;
+    String CODICEIDENTIFICATIVO;
+    String DATAULTIMAMODIFICA;
+    String CLASSIFICAZIONE;
+}
+
--- app/src/main/java/com/example/is_poi/MainActivity.java
+++ app/src/main/java/com/example/is_poi/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.UserData;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,18 +12,74 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+public class MainActivity extends AppCompatActivity {
+    interface RequestUser{
+        @GET("/export/json/Elenco-delle-Strutture-Ricettive-Turistiche-della-Regione-Veneto.json")
+        Call<ArrayList<Alberghi>> getUser();
+    }
     FirebaseAuth auth;
     FirebaseUser user;
+    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button button;
-        TextView textView;
+
         FirebaseUser user;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView textView;
+        textView= findViewById((R.id.user_details));
+        /* https://www.veneto.eu/static/opendata/dove-alloggiare.csv*/
+        // https://dati.veneto.it/export/json/Elenco-delle-Strutture-Ricettive-Turistiche-della-Regione-Veneto.json
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder().callTimeout(2, TimeUnit.MINUTES).connectTimeout(2, TimeUnit.MINUTES).readTimeout(2, TimeUnit.MINUTES).writeTimeout(2, TimeUnit.MINUTES);
+
+
+        Retrofit retrofit= new Retrofit.Builder().baseUrl("https://dati.veneto.it").addConverterFactory(GsonConverterFactory.create()).client(httpClient.build()).build();
+
+        RequestUser ru= retrofit.create(RequestUser.class);
+
+        TextView finalTextView = textView;
+        ru.getUser().enqueue(new Callback<ArrayList<Alberghi>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Alberghi>> call, Response<ArrayList<Alberghi>> response) {
+                finalTextView.setText(response.body().get(12).COMUNE);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Alberghi>> call, Throwable t) {
+                finalTextView.setText(t.getMessage());
+            }
+        });
+
+
 
         auth=FirebaseAuth.getInstance();
         button=findViewById(R.id.logout);
