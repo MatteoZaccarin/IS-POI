5c630880e09409b914dc739172e275582c8df9e7
fabio-12 <128527959+fabio-12@users.noreply.github.com>
2023-12-13 15:45:19 +0100

    prima parte search poi completata

--- app/src/main/java/com/example/is_poi/Search_poi.java
+++ app/src/main/java/com/example/is_poi/Search_poi.java
@@ -2,11 +2,16 @@ package com.example.is_poi;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Search_poi extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +65,16 @@ public class Search_poi extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_search_poi, container, false);
+    }
+
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        String[] s=getResources().getStringArray(R.array.poi_type);
+        ArrayAdapter<String> arrayApp=new ArrayAdapter<String>(requireContext(), R.layout.dropdown_item, s);
+        AutoCompleteTextView textView= (AutoCompleteTextView) view.findViewById(R.id.autoCompleteTextView2);
+        textView.setAdapter(arrayApp);
+
     }
 }
