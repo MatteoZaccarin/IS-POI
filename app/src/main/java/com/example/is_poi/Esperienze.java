2b1e94db2c6a67f666e426886a33da08d6cf5490
fabio-12 <128527959+fabio-12@users.noreply.github.com>
2023-12-13 11:04:06 +0100

    classi per API

---
+++ app/src/main/java/com/example/is_poi/Esperienze.java
@@ -0,0 +1,37 @@
+package com.example.is_poi;
+
+import com.google.gson.annotations.SerializedName;
+
+/*
+RoadBike -> https://dati.veneto.it/export/json/Percorsi-Road-Bike-nel-Veneto.json
+Scii di fondo -> https://dati.veneto.it/export/json/Percorsi-Sci-di-Fondo-nel-Veneto.json
+Mountain Bike -> https://dati.veneto.it/export/json/Percorsi-Mountain-Bike-nel-Veneto.json
+
+
+
+*/
+
+public class Esperienze {
+    String Categoria;
+    String Titolo;
+    String Sintesi;
+    String Descrizione;
+    @SerializedName("Consigli dell'autore")
+    String consigliAutore;
+    @SerializedName("Informazioni sulla Sicurezza")
+    String informazioniSicurezza;
+    String Equipaggiamento;
+    @SerializedName("Consigli suggerimenti e link utili")
+    String consigli;
+    @SerializedName("Punto di partenza")
+    String PuntoDiPartenza;
+    @SerializedName("Punto di arrivo")
+    String PuntoDiArrivo;
+    @SerializedName("Tutto l'anno")
+    String tuttoAnno;
+
+    String Gennaio;String Febbraio;String Marzo;String Aprile;String Maggio;String Giugno;
+    String Luglio;String Agosto;String Settembre;String Ottobre;String Novembre;String Dicembre;
+
+
+}
---
+++ app/src/main/java/com/example/is_poi/SentieriPanoramici.java
@@ -0,0 +1,32 @@
+package com.example.is_poi;
+
+import com.google.gson.annotations.SerializedName;
+/*
+piste ciclabili -> https://dati.veneto.it/export/json/Percorsi-Ciclabili-Panoramici-nel-Veneto.json
+sentieri storici -> https://dati.veneto.it/export/json/Sentieri-Storico-Culturali-e-Panoramici-nel-Veneto.json
+sentieri enogastronomici -> https://dati.veneto.it/export/json/Sentieri-Enogastronomici-nel-Veneto.json
+*/
+public class SentieriPanoramici {
+    String Categoria;
+    @SerializedName("Link al GPX/KML")
+    String link;
+    String Titolo;
+    String Sintesi;
+    String Descrizione;
+    @SerializedName("Consigli dell'autore")
+    String consigliAutore;
+    @SerializedName("Informazioni sulla Sicurezza")
+    String informazioniSicurezza;
+    String Equipaggiamento;
+    @SerializedName("Consigli suggerimenti e link utili")
+    String consigli;
+    @SerializedName("Punto di partenza")
+    String PuntoDiPartenza;
+    @SerializedName("Punto di arrivo")
+    String PuntoDiArrivo;
+    @SerializedName("Tutto l'anno")
+    String tuttoAnno;
+
+    String Gennaio;String Febbraio;String Marzo;String Aprile;String Maggio;String Giugno;
+    String Luglio;String Agosto;String Settembre;String Ottobre;String Novembre;String Dicembre;
+}
