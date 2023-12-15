8042842a287ddc438c13834bcd339aad77fc02f8
MatteoZaccarin <119884615+MatteoZaccarin@users.noreply.github.com>
2023-12-15 17:12:24 +0100

    sistemata posizione view

--- app/src/main/java/com/example/is_poi/Logout.java
+++ app/src/main/java/com/example/is_poi/Logout.java
@@ -35,44 +35,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class Logout extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Logout() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Logout.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Logout newInstance(String param1, String param2) {
-        Logout fragment = new Logout();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-
     }
 
     @Override
@@ -87,28 +52,7 @@ public class Logout extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Button button=view.findViewById(R.id.logout);
 
-        TextView textView=view.findViewById(R.id.user_details);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder().callTimeout(2, TimeUnit.MINUTES).connectTimeout(2, TimeUnit.MINUTES).readTimeout(2, TimeUnit.MINUTES).writeTimeout(2, TimeUnit.MINUTES);
 
-
-        Retrofit retrofit= new Retrofit.Builder().baseUrl("https://dati.veneto.it").addConverterFactory(GsonConverterFactory.create()).client(httpClient.build()).build();
-
-        MainActivity.RequestAlberghi ru= retrofit.create(MainActivity.RequestAlberghi.class);
-
-        TextView finalTextView = textView;
-
-        ru.getAlberghi().enqueue(new retrofit2.Callback<ArrayList<Alberghi>>() {
-
-            @Override
-            public void onResponse(Call<ArrayList<Alberghi>> call, Response<ArrayList<Alberghi>> response) {
-                finalTextView.setText(response.body().get(12).COMUNE);
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<Alberghi>> call, Throwable t) {
-                finalTextView.setText(t.getMessage());
-            }
-        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/is_poi/Search_city.java
+++ app/src/main/java/com/example/is_poi/Search_city.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,9 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -26,7 +32,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Search_city extends Fragment {
-
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout,fragment);
+        fragmentTransaction.commit();
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,17 +66,33 @@ public class Search_city extends Fragment {
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line,nomiComuni);
                 AutoCompleteTextView textView = (AutoCompleteTextView) view.findViewById(R.id.autoCompleteTextView);
                 textView.setAdapter(adapter);
+
+                Button b=view.findViewById(R.id.Cerca_comune);
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Boolean find=false;
+                        for (String comune:nomiComuni) {
+                            if (comune.equals(textView.getText().toString())){
+                                find = true;
+                                break;
+                            } else
+                                Log.d(comune, textView.getText().toString());
+                        }
+                        if(find)
+                            replaceFragment(new City_risultati());
+                        else{
+                            Toast errorToast = Toast.makeText(getActivity(), "Inserisci un comune presente in Veneto, selezionalo dai consigli automatici!", Toast.LENGTH_SHORT);
+                            errorToast.show();
+                        }
+
+                    }
+                });
             }
             @Override
             public void onFailure(Call<ArrayList<Comuni>> call, Throwable t) {
                 return;
             }
         });
-
-
-
-
-
-
     }
 }
