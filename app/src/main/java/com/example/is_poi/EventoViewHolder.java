e63bc17d603fcc25ba783b6b7c2cb7ddd1ce2607
fabio-12 <128527959+fabio-12@users.noreply.github.com>
2024-05-19 14:43:46 +0200

    fatto anche la cancellazione problema dei dati non live
    valutare se rifare con i view model

--- app/src/main/java/com/example/is_poi/EventoViewHolder.java
+++ app/src/main/java/com/example/is_poi/EventoViewHolder.java
@@ -1,14 +1,23 @@
 package com.example.is_poi;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Date;
 
@@ -16,6 +25,7 @@ public class EventoViewHolder extends RecyclerView.ViewHolder {
     TextView nomeEvento, Indirizzo, Date, Descrizione, Id;
     Button editButton;
     ImageButton deleteButton;
+    EventiAdapter adapter;
     public EventoViewHolder(@NonNull View itemView, Context context) {
         super(itemView);
         nomeEvento=itemView.findViewById(R.id.nomeEvento);
@@ -25,7 +35,6 @@ public class EventoViewHolder extends RecyclerView.ViewHolder {
         Id=itemView.findViewById(R.id.id);
         editButton=itemView.findViewById(R.id.modify);
         deleteButton=itemView.findViewById(R.id.cancel);
-
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,10 +45,37 @@ public class EventoViewHolder extends RecyclerView.ViewHolder {
         });
 
 
+        EventiAdapter finalAdapter = adapter;
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //cancella evento
+                AlertDialog.Builder builder=new AlertDialog.Builder(context);
+                builder.setTitle("Cancellare "+nomeEvento.getText().toString()+"?");
+                builder.setMessage("Sei sicuro di volevr cancellare questo evento?");
+                builder.setPositiveButton("Conferma", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatabaseReference dbRef = FirebaseDatabase.getInstance("https://is-poi-default-rtdb.europe-west1.firebasedatabase.app").getReference("eventi/"+Id.getText().toString());
+                        dbRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>(){
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(context, "Evento cancellato con successo", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(context, "Errore nella cancellazione del evento, RIPROVA", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                builder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                builder.show();
             }
         });
     }
